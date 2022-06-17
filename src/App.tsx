5dcf19f30b71ea4e95bd40d95af5f0af018c7e16
KenParsons <102046272+KenParsons@users.noreply.github.com>
2022-06-17 17:30:06 -0400

    add: first draft working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
+import movingCouple from "./couple_with_two_children_moving-1800-1200.webp"
 import './App.css';
 import ParallaxImage, { ParallaxImageProps } from "./components/ParallaxImage"
 
 function App() {
-  const containerRef = useRef<HTMLDivElement>(null);
 
 
   return (
     <div
       className="App"
-      style={{ display: "flex", flexDirection: "column", minHeight: "150vh", justifyContent: "center" }}>
+      style={{ display: "flex", flexDirection: "column", minHeight: "500vh", justifyContent: "flex-start" }}>
 
       <header className="App-header">
 
         <ParallaxImage
-        viewportHeight={500}
-          containerRef={containerRef}
-          containerStyle={{
-            boxShadow: "inset 0px 0px 7px rgba(0,0,0,0.7)",
-            borderRadius: 6,
-            height: "800px"
-          }}
-          height={750}
-          src={logo}
-          className="App-logo"
-          alt="logo"
+          width={"100%"}
+          src={movingCouple}
         />
 
       </header>
--- src/components/ParallaxImage.tsx
+++ src/components/ParallaxImage.tsx
@@ -9,26 +9,30 @@ export interface ParallaxImageProps extends React.DetailedHTMLProps<React.ImgHTM
 const lockedContainerStyles: CSSProperties = {
     overflow: "hidden",
     width: "fit-content",
-
     height: "viewportHeight"
 }
 
 export default function ParallaxImage(props: ParallaxImageProps) {
-    const [height, setHeight] = useState( validateAndGetHeight(props) );
-    if (!height) { throw Error('Parallax Image: Either the height prop, or a value for style.height is required.') }
+    const [height, setHeight] = useState(validateAndGetHeight(props));
+    const [waitForImageToLoad, setWaitForImageToLoad] = useState(!!height)
+    if (waitForImageToLoad && isDev()) { 
+        console.warn(`Image height was not explicitly passed. `)
+    }
 
-    const containerStyle: CSSProperties = {...props.containerStyle, ...lockedContainerStyles};
+    const roomToSlide = getRoomToSlide(height, props.viewportHeight)
+
+    const containerStyle: CSSProperties = { ...props.containerStyle, ...lockedContainerStyles };
     containerStyle.height = props.viewportHeight
+
     const [loadStarted, setLoadStarted] = useState(false);
     const [loadFinished, setLoadFinished] = useState(false)
     const imageRef = useRef<HTMLImageElement>(null);
 
-    useEffect(()=>{ setLoadStarted(true) }, []);
+    useEffect(() => { setLoadStarted(true) }, []);
     useEffect(load, [loadStarted])
 
     function load() {
         if (!loadStarted) { return }
-
         warnUserIfStylesOverridden(props.containerStyle);
         const img = imageRef.current as HTMLImageElement;
         window.addEventListener('scroll', (event) => { handleScroll(event, img) })
@@ -36,12 +40,14 @@ export default function ParallaxImage(props: ParallaxImageProps) {
     }
 
     function handleScroll(event: Event, img: HTMLImageElement) {
-        const percentageOfPageScrolled = window.scrollY / (document.documentElement.offsetHeight - window.innerHeight);
-
+        const container = img.parentElement as HTMLDivElement
+        const percentageScrolled = getPercentageScrolled(container);
+        const pxToSlide = (percentageScrolled * roomToSlide) * -1;
+        updateTransform(img, "translateY", `${pxToSlide}px`)
     }
 
     //We have to remove a few items from our props before passing because React won't recognize those as a DOM element prop
-    //The Partial<> is because containerStyle is required
+    //The Partial<> is because viewportHeight is required
     const imgPropsToPass: Partial<ParallaxImageProps> = { ...props }
     delete imgPropsToPass.containerRef;
     delete imgPropsToPass.containerStyle;
@@ -68,9 +74,30 @@ export default function ParallaxImage(props: ParallaxImageProps) {
     )
 }
 
+function getRoomToSlide(height: string | number | null, viewportHeight: number | `${number}px` | `${number}%`): number {
+    const pixelsHeight = !height ? 0
+        : (typeof height === "number") ? height
+            : height.includes('px') ? parseInt(height) : Number(height);
+
+    if (!pixelsHeight || Number.isNaN(pixelsHeight)) {
+        throw Error('Height was not passed as either a positive number or a string with "{positiveNumber}px" format');
+    }
+
+    const viewportPixelsHeight = (typeof viewportHeight === "number") ? viewportHeight
+        : viewportHeight.includes("px") ? parseInt(viewportHeight)
+            : (parseInt(viewportHeight) / 100) * pixelsHeight
+
+
+    if (viewportHeight > pixelsHeight) {
+        throw Error('Viewport height larger than height. Use default viewportheight, like 75% of height or something');
+    };
+    return pixelsHeight - viewportPixelsHeight
+}
+
+
 function warnUserIfStylesOverridden(appliedStyles: { [key: string]: any } | undefined) {
-    if (!appliedStyles) { return }
-    
+    if (!appliedStyles || !isDev()) { return }
+
     const locked = lockedContainerStyles as { [key: string]: any }
     for (const style in locked) {
         if (appliedStyles[style]) {
@@ -87,13 +114,120 @@ function validateAndGetHeight(props: ParallaxImageProps) {
     if (!height) { return null };
 
     if (typeof height === "string") {
-        if (!height.includes("%") && !height.includes("px") && Number.isNaN(Number(height)) ) { 
-            return null 
+        if (!height.includes("%") && !height.includes("px") && Number.isNaN(Number(height))) {
+            return null
         }
 
     }
 
 
-
     return height;
+}
+
+function elementIsAboveTheFold(element: HTMLElement) {
+    return element.offsetTop < window.innerHeight
+}
+
+function elementIsOnBottomFold(element: HTMLElement) {
+    return document.documentElement.offsetHeight - window.innerHeight < (element.offsetHeight + element.offsetTop);
+}
+
+function isDev() {
+    return window.location.href.includes("localhost:") || window.location.href.includes("127.0.0.1");
+}
+
+
+type ImagePosition =
+    | "middle"
+    | "top"
+    | "bottom"
+    | "alwaysVisible"
+
+function getPercentageScrolled(container: HTMLElement): number {
+    const aboveFold = elementIsAboveTheFold(container);
+    const isOnBottomFold = elementIsOnBottomFold(container);
+
+    if (!aboveFold && !isOnBottomFold) {
+        return percentageScrolledByImagePosition["middle"](container)
+    }
+    if (aboveFold && !isOnBottomFold) {
+        return percentageScrolledByImagePosition["top"](container);
+    }
+
+    if (!aboveFold && isOnBottomFold) {
+        return percentageScrolledByImagePosition["bottom"](container);
+    }
+
+    return percentageScrolledByImagePosition["alwaysVisible"](container)
+}
+
+type FunctionByImagePosition = {
+    [Position in ImagePosition]: Function
+}
+
+const percentageScrolledByImagePosition: FunctionByImagePosition = {
+    "middle": (element: HTMLElement) => {
+        const pxScrolled = (Math.ceil(window.scrollY) + window.innerHeight) - element.offsetTop;
+        const percentageScrolled = (pxScrolled / (window.innerHeight + element.offsetHeight));
+        return getClampedPercentage(percentageScrolled)
+    },
+    "top": (element: HTMLElement) => {
+        const percentageScrolled = Math.ceil(window.scrollY) / (element.offsetTop + element.offsetHeight)
+        return getClampedPercentage(percentageScrolled)
+    },
+    "bottom": (element: HTMLElement) => {
+        const percentageScrolled = (Math.ceil(window.scrollY + window.innerHeight) - element.offsetTop) /
+            (document.documentElement.scrollHeight - element.offsetTop)
+        return getClampedPercentage(percentageScrolled)
+    },
+    "alwaysVisible": (element: HTMLElement) => {
+        const percentageScrolled = Math.ceil(window.scrollY) / (document.documentElement.scrollHeight - window.innerHeight);
+        return getClampedPercentage(percentageScrolled);
+    }
+}
+
+function getClampedPercentage(percentage: number) {
+    return percentage < 0 ? 0 : percentage > 1 ? 1 : percentage
+}
+
+
+type TransformProperty =
+    | "translate"
+    | "translateX"
+    | "translateY"
+    | "translateZ"
+    | "translate3d"
+    | "skew"
+    | "skewX"
+    | "skewY"
+    | "scale"
+    | "scaleX"
+    | "scaleY"
+    | "rotate"
+    | "rotate3d"
+    | "rotateX"
+    | "rotateY"
+    | "rotateZ"
+    | "perspective"
+    | "matrix"
+    | "matrix3d"
+
+
+function updateTransform(element: HTMLElement, transformProperty: TransformProperty, transformValue: string) {
+    const propertyRegex = new RegExp(transformProperty + "\\((.*?)\\)", "i");
+    const matches = element.style.transform.match(propertyRegex);
+    const updatedValue = `${transformProperty}(${transformValue})`
+    if (!matches) {
+        element.style.transform += ' ' + updatedValue
+    } else {
+        const replaced = element.style.transform.replace(propertyRegex, updatedValue)
+        element.style.transform = replaced;
+    }
+}
+
+function getTransformValue(element: HTMLElement, transformProperty: TransformProperty) {
+    const propertyRegex = new RegExp(transformProperty + "\\((.*?)\\)", "i");
+    const matches = element.style.transform.match(propertyRegex);
+    if (!matches) { return null };
+    return matches[1];
 }
